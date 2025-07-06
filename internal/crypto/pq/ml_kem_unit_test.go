1d9d162195cf86059905e0078f5ee0db6337f31e
Justin Cranford <justincranford@hotmail.com>
2025-07-05 22:39:44 -0400

    Added more validation in ML KEM tests

--- internal/crypto/pq/ml_kem_unit_test.go
+++ internal/crypto/pq/ml_kem_unit_test.go
@@ -7,47 +7,49 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGenerateMLKEMKeyPair(t *testing.T) {
+func TestMarshallUnmarshallMLKEMKeyPair(t *testing.T) {
 	keyPair, err := GenerateMLKEMKeyPair()
 	require.NoError(t, err, "failed to generate ML-KEM key pair")
-	require.NotNil(t, keyPair.PublicKey, "public key should not be nil")
-	require.NotNil(t, keyPair.PrivateKey, "private key should not be nil")
+	require.NotNil(t, keyPair, "expected non-nil key pair")
+	require.NotNil(t, keyPair.PublicKey, "expected non-nil public key")
+	require.NotNil(t, keyPair.PrivateKey, "expected non-nil private key")
 
 	publicKeyBytes, err := MarshalPublicKey(keyPair.PublicKey)
-	require.NoError(t, err, "marshal public key failed")
-	require.NotEmpty(t, publicKeyBytes, "public key bytes should not be empty")
+	require.NoError(t, err, "failed to marshal public key")
+	require.NotEmpty(t, publicKeyBytes, "expected non-empty public key bytes")
 
 	privateKeyBytes, err := MarshalPrivateKey(keyPair.PrivateKey)
-	require.NoError(t, err, "marshal private key failed")
-	require.NotEmpty(t, publicKeyBytes, "private key bytes should not be empty")
+	require.NoError(t, err, "failed to marshal private key")
+	require.NotEmpty(t, publicKeyBytes, "expected non-empty private key bytes")
 
 	publicKey, err := UnmarshalPublicKey(publicKeyBytes)
 	require.NoError(t, err, "failed to unmarshall public key")
-	require.NotNil(t, publicKey, "public key should not be nil")
+	require.NotNil(t, keyPair.PublicKey, "expected non-nil public key")
+	require.True(t, publicKey.Equal(keyPair.PublicKey), "expected unmarshalled public key to match the original public key")
 
 	privateKey, err := UnmarshalPrivateKey(privateKeyBytes)
 	require.NoError(t, err, "failed to unmarshall private key")
-	require.NotNil(t, privateKey, "private key should not be nil")
+	require.NotNil(t, keyPair.PrivateKey, "expected non-nil private key")
+	require.True(t, privateKey.Equal(keyPair.PrivateKey), "expected unmarshalled private key to match the original private key")
 }
 
 func TestMLKEMEncapsulateAndDecapsulate(t *testing.T) {
 	keyPair, err := GenerateMLKEMKeyPair()
 	require.NoError(t, err, "failed to generate ML-KEM key pair")
-	require.NotNil(t, keyPair.PublicKey, "public key should not be nil")
-	require.NotNil(t, keyPair.PrivateKey, "private key should not be nil")
+	require.NotNil(t, keyPair, "expected non-nil key pair")
+	require.NotNil(t, keyPair.PublicKey, "expected non-nil public key")
+	require.NotNil(t, keyPair.PrivateKey, "expected non-nil private key")
 
 	ciphertext, sharedSecret, err := MLKEMEncapsulate(keyPair.PublicKey)
 	require.NoError(t, err, "failed to encapsulate public key")
-	require.NotEmpty(t, ciphertext, "ciphertext should not be empty")
-	require.NotEmpty(t, sharedSecret, "sharedSecret should not be empty")
+	require.NotEmpty(t, ciphertext, "expected non-empty ciphertext")
+	require.NotEmpty(t, sharedSecret, "expected non-empty sharedSecret")
 
 	sharedSecret2, err := MLKEMDecapsulate(keyPair.PrivateKey, ciphertext)
 	require.NoError(t, err, "failed to decapsulate ciphertext with private key")
-	require.NotEmpty(t, sharedSecret2, "sharedSecret2 should not be empty")
+	require.NotEmpty(t, sharedSecret2, "expected non-empty sharedSecret2")
 
-	if subtle.ConstantTimeCompare(sharedSecret, sharedSecret2) == 0 {
-		t.Error("shared secrets were expected to match, decapculate with private key and ciphertext should have returned a same shared secret as encapculate with public key")
-	}
+	require.NotZero(t, subtle.ConstantTimeCompare(sharedSecret, sharedSecret2), "expected shared secrets to match")
 }
 
 func TestMLKEMEncapsulateWithInvalidKey(t *testing.T) {
@@ -71,46 +73,45 @@ func TestMLKEMDecapsulateWithInvalidKey(t *testing.T) {
 func TestMLKEMDecapsulateWithTamperedCiphertext(t *testing.T) {
 	keyPair, err := GenerateMLKEMKeyPair()
 	require.NoError(t, err, "failed to generate ML-KEM key pair")
-	require.NotNil(t, keyPair.PublicKey, "public key should not be nil")
-	require.NotNil(t, keyPair.PrivateKey, "private key should not be nil")
+	require.NotNil(t, keyPair, "expected non-nil key pair")
+	require.NotNil(t, keyPair.PublicKey, "expected non-nil public key")
+	require.NotNil(t, keyPair.PrivateKey, "expected non-nil private key")
 
 	ciphertext, sharedSecret, err := MLKEMEncapsulate(keyPair.PublicKey)
 	require.NoError(t, err, "failed to encapsulate public key")
-	require.NotEmpty(t, ciphertext, "ciphertext should not be empty")
-	require.NotEmpty(t, sharedSecret, "sharedSecret should not be empty")
+	require.NotEmpty(t, ciphertext, "expected non-empty ciphertext")
+	require.NotEmpty(t, sharedSecret, "expected non-empty sharedSecret")
 
 	ciphertext[0] ^= 0xFF // tamper ciphertext
 
 	sharedSecretTampered, err := MLKEMDecapsulate(keyPair.PrivateKey, ciphertext)
 	require.NoError(t, err, "failed to decapsulate ciphertext with private key")
-	require.NotEmpty(t, sharedSecretTampered, "sharedSecretTampered should not be empty")
+	require.NotEmpty(t, sharedSecretTampered, "expected non-empty sharedSecretTampered")
 
-	if subtle.ConstantTimeCompare(sharedSecret, sharedSecretTampered) == 1 {
-		t.Error("shared secrets were expected to be different, decapsulated with tampered ciphertext should have returned a different shared secret")
-	}
+	require.Zero(t, subtle.ConstantTimeCompare(sharedSecret, sharedSecretTampered), "expected different shared secret for decapsulate with tampered ciphertext")
 }
 
 func TestMLKEMDecapsulateWithWrongKey(t *testing.T) {
 	keyPair1, err := GenerateMLKEMKeyPair()
-	require.NoError(t, err, "failed to generate ML-KEM key pair")
-	require.NotNil(t, keyPair1.PublicKey, "public key 1 should not be nil")
-	require.NotNil(t, keyPair1.PrivateKey, "private key 1 should not be nil")
+	require.NoError(t, err, "failed to generate ML-KEM key pair 1")
+	require.NotNil(t, keyPair1, "expected non-nil key pair 1")
+	require.NotNil(t, keyPair1.PublicKey, "expected non-nil public key 1")
+	require.NotNil(t, keyPair1.PrivateKey, "expected non-nil private key 1")
 
 	keyPair2, err := GenerateMLKEMKeyPair()
-	require.NoError(t, err, "failed to generate ML-KEM key pair")
-	require.NotNil(t, keyPair2.PublicKey, "public key 2 should not be nil")
-	require.NotNil(t, keyPair2.PrivateKey, "private key 2 should not be nil")
+	require.NoError(t, err, "failed to generate ML-KEM key pair 2")
+	require.NotNil(t, keyPair2, "expected non-nil key pair 2")
+	require.NotNil(t, keyPair2.PublicKey, "expected non-nil public key 2")
+	require.NotNil(t, keyPair2.PrivateKey, "expected non-nil private key 2")
 
 	ciphertext, sharedSecret, err := MLKEMEncapsulate(keyPair1.PublicKey)
 	require.NoError(t, err, "failed to encapsulate public key 1")
-	require.NotEmpty(t, ciphertext, "ciphertext should not be empty")
-	require.NotEmpty(t, sharedSecret, "sharedSecret should not be empty")
+	require.NotEmpty(t, ciphertext, "expected non-empty ciphertext")
+	require.NotEmpty(t, sharedSecret, "expected non-empty sharedSecret")
 
 	sharedSecretWrong, err := MLKEMDecapsulate(keyPair2.PrivateKey, ciphertext)
 	require.NoError(t, err, "failed to decapsulate ciphertext with private key 2")
-	require.NotEmpty(t, sharedSecretWrong, "sharedSecretWrong should not be empty")
+	require.NotEmpty(t, sharedSecretWrong, "expected non-empty sharedSecretWrong")
 
-	if subtle.ConstantTimeCompare(sharedSecret, sharedSecretWrong) == 1 {
-		t.Error("shared secrets were expected to be different, decapsulated with wrong private key should have returned a different shared secret")
-	}
+	require.Zero(t, subtle.ConstantTimeCompare(sharedSecret, sharedSecretWrong), "expected different shared secret for decapsulate with wrong private key")
 }
