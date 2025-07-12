59e8160d93c08b48c904cff2fba98ef62152237f
Justin Cranford <justincranford@hotmail.com>
2025-07-12 16:24:13 -0400

    Fixed MLKEM benchmarks

--- pq/ml_kem_bench_test.go
+++ pq/ml_kem_bench_test.go
@@ -12,10 +12,10 @@ func BenchmarkMLKEMEncapsulate(b *testing.B) {
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			sharedSecret, ciphertext, err := MLKEMEncapsulate(keyPair.PublicKey)
+			ciphertext, sharedSecret, err := MLKEMEncapsulate(keyPair.PublicKey)
 			require.NoError(b, err, "Encapsulation should not error")
-			require.NotNil(b, sharedSecret, "Shared secret should not be nil")
 			require.NotNil(b, ciphertext, "Ciphertext should not be nil")
+			require.NotNil(b, sharedSecret, "Shared secret should not be nil")
 		}
 	})
 }
@@ -23,14 +23,21 @@ func BenchmarkMLKEMEncapsulate(b *testing.B) {
 func BenchmarkMLKEMDecapsulate(b *testing.B) {
 	keyPair, err := GenerateMLKEMKeyPair()
 	require.NoError(b, err, "Key pair generation should not error")
-	sharedSecret, ciphertext, err := MLKEMEncapsulate(keyPair.PublicKey)
+
+	ciphertext, sharedSecret, err := MLKEMEncapsulate(keyPair.PublicKey)
 	require.NoError(b, err, "Encapsulation should not error")
-	require.NotNil(b, sharedSecret, "Shared secret should not be nil")
 	require.NotNil(b, ciphertext, "Ciphertext should not be nil")
+	require.NotNil(b, sharedSecret, "Shared secret should not be nil")
+
 	b.ResetTimer()
+
+	// Use the same ciphertext for all iterations
 	b.RunParallel(func(pb *testing.PB) {
+		localCiphertext := make([]byte, len(ciphertext))
+		copy(localCiphertext, ciphertext)
+
 		for pb.Next() {
-			recoveredSecret, err := MLKEMDecapsulate(keyPair.PrivateKey, ciphertext)
+			recoveredSecret, err := MLKEMDecapsulate(keyPair.PrivateKey, localCiphertext)
 			require.NoError(b, err, "Decapsulation should not error")
 			require.NotNil(b, recoveredSecret, "Recovered secret should not be nil")
 		}
